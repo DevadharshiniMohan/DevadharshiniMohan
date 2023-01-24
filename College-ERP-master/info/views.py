8abc581296d498720026ea294326609ba5a09c55
Dharshini <dharshini@riverstonetech.com>
2023-01-24 18:19:08 +0530

    Update

--- College-ERP-master/info/views.py
+++ College-ERP-master/info/views.py
@@ -465,6 +465,7 @@ def update_officestaff(request, ID):
 def delete_officestaff(request, ID):
 
     delete_staff = Office_staff.objects.get(ID=ID)
+    print(delete_staff)
 
     if request.method == 'POST':
         delete_staff.delete()
